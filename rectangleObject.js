806668a21bd2fe313528f4ee00390bfb9dc14d15
Lucas Medina <lucasmedinarmc@gmail.com>
2024-03-18 20:54:02 -0300

    feat: add challenge answer

--- rectangleObject.js
+++ rectangleObject.js
@@ -1,3 +1,17 @@
-function Rectangle(a, b) {}
+function Rectangle(a, b) {
+  const length = a
+  const width = b
+  const perimeter = 2 * (length + width)
+  const area = length * width
+
+  let rectangleObj = {
+    length,
+    width,
+    perimeter,
+    area
+  }
+
+  return rectangleObj
+}
 
 Rectangle(4, 5)
