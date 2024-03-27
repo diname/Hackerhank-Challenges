5abccc642ef6768578d18f2e8bfc83a058fa44d0
Lucas Medina <lucasmedinarmc@gmail.com>
2024-03-26 21:32:39 -0300

    feat: add the answer of challenge

--- javascriptDates.js
+++ javascriptDates.js
@@ -1,42 +1,52 @@
-'use strict';
+'use strict'
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.resume()
+process.stdin.setEncoding('utf-8')
 
-let inputString = '';
-let currentLine = 0;
+let inputString = ''
+let currentLine = 0
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
+process.stdin.on('data', (inputStdin) => {
+  inputString += inputStdin
+})
 
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
+process.stdin.on('end', (_) => {
+  inputString = inputString
+    .trim()
+    .split('\n')
+    .map((string) => {
+      return string.trim()
+    })
+
+  main()
+})
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++]
 }
 
 // The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
 function getDayName(dateString) {
-    let dayName;
-    // Write your code here
-    
-    return dayName;
+  // Write your code here
+  let daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ]
+  let day = new Date(dateString).getDay()
+  return daysOfWeek[day]
 }
 
-
 function main() {
-    const d = +(readLine());
-    
-    for (let i = 0; i < d; i++) {
-        const date = readLine();
-        
-        console.log(getDayName(date));
-    }
+  const d = +readLine()
+
+  for (let i = 0; i < d; i++) {
+    const date = readLine()
+
+    console.log(getDayName(date))
+  }
 }
